636be8e06e9690e8112f91516dece48f9cd05152
spaceengineer56 <spaceengineer95@gmail.com>
2024-01-06 14:21:12 +0500

    add separate three stage and improved two stage separate

--- rocket.py
+++ rocket.py
@@ -35,6 +35,12 @@ buster_img_right = buster_image_right[0]
 buster_x_right = width // 2 - buster_img_right.get_width() // 2
 buster_y_right = height // 2 - buster_img_right.get_height() // 2
 
+stage_image = [pygame.image.load(os.path.join("data", "angara_stage_sep.jpg")), ]
+
+stage_img = stage_image[0]
+stage_x = width // 2 - stage_img.get_width() // 2
+stage_y = height // 2 - stage_img.get_height() // 2
+
 
 # Настройка часов
 clock = pygame.time.Clock()
@@ -43,8 +49,10 @@ clock = pygame.time.Clock()
 animation_time = 0.02
 last_time = 0
 buster_angle_yaw = 0
+stage_angle_variable = 0
 buster_angle_yaw_right = 0
 flag_buster = False
+flag_stage = False
 buster_angle_variable = 0
 buster_angle_variable_right = 0
 
@@ -64,6 +72,7 @@ pygame.mixer.music.play(-1)  # -1 для зацикливания воспрои
 running = True
 music_playing = True  # Добавляем флаг для отслеживания воспроизведения музыки
 buster_offset = 0  # Начальное смещение бустера
+stage_offset = 0  # Начальное смещение ступени
 buster_offset_right = 0  # Начальное смещение правого бустера
 while running:
     win.fill(background_color)
@@ -84,6 +93,8 @@ while running:
             rocket_img = random.choice([pygame.image.load(os.path.join("data", "angara_three_stage_1.jpg")),
                                         pygame.image.load(os.path.join("data", "angara_three_stage_2.jpg")),
                                         pygame.image.load(os.path.join("data", "angara_three_stage_3.jpg"))])
+
+            flag_stage = True
         else:
             rocket_img = random.choice(rocket_images)
         last_time = time.time()
@@ -103,21 +114,22 @@ while running:
     win.blit(velocity_text, (width - velocity_text.get_width() - 10, 10))
 
     if flag_buster:
+        buster_angle_yaw = angle_yaw + buster_angle_variable
+        buster_angle_variable += 0.5
         buster_sep = pygame.transform.rotate(buster_img, buster_angle_yaw)
         buster_rect = buster_sep.get_rect(
             center=(buster_x + buster_img.get_width() // 2, buster_y + buster_img.get_height() // 2))
         win.blit(buster_sep, buster_rect.topleft)
-        buster_angle_yaw = angle_yaw + buster_angle_variable
-        buster_angle_variable += 1
 
+        buster_angle_yaw_right = angle_yaw + buster_angle_variable_right
+        buster_angle_variable_right -= 0.5
         buster_sep_right = pygame.transform.rotate(buster_img_right, buster_angle_yaw_right)
         buster_rect_right = buster_sep_right.get_rect(
             center=(
                 buster_x_right + buster_img_right.get_width() // 2,
                 buster_y_right + buster_img_right.get_height() // 2))
         win.blit(buster_sep_right, buster_rect_right.topleft)
-        buster_angle_yaw_right = angle_yaw + buster_angle_variable_right
-        buster_angle_variable_right -= 1
+
 
         if buster_y < height:
             buster_y += 1  # Движение бустера вниз
@@ -129,8 +141,20 @@ while running:
             buster_x -= 1  # Движение бустера влево
             buster_offset += 1  # Увеличение смещения бустера
         if buster_x_right < width - buster_img_right.get_width():
-            buster_x_right += 1  # Движение правого бустера вправо
-            buster_offset_right += 1  # Увеличение смещения правого бустера
+            buster_x_right += 0  # Движение правого бустера вправо
+            buster_offset_right += 0  # Увеличение смещения правого бустера
+    if flag_stage:
+        stage_angle_yaw = angle_yaw + stage_angle_variable
+        stage_angle_variable += 0.5
+        stage_sep = pygame.transform.rotate(stage_img,stage_angle_yaw)
+        stage_rect = stage_sep.get_rect(
+            center=(stage_x + stage_img.get_width() // 2, stage_y + stage_img.get_height() // 2))
+        win.blit(stage_sep, stage_rect.topleft)
+        if stage_y < height:
+            stage_y += 1
+        if stage_x > 0:
+            stage_x -= 1  # Движение бустера влево
+            stage_offset += 1  # Увеличение смещения бустера
     # Обновление дисплея
     pygame.display.update()
 
@@ -142,9 +166,11 @@ while running:
     # Управление вращением ракеты
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        angle_yaw += 0.15
+        if angle_yaw < 90:
+            angle_yaw += 0.15
     if keys[pygame.K_d]:
-        angle_yaw -= 0.15
+        if angle_yaw > -90:
+            angle_yaw -= 0.15
 
     # Обновление скорости с ускорением
     if velocity < 11:
